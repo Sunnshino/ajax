287e98b3258c83163f9dd12b1fe019b3424724fe
Sunnshino <1940694428@qq.com>
2017-10-22 18:07:37 +0800

    提交基础封装ajax

---
+++ ajax.js
@@ -0,0 +1,46 @@
+function ajax(opt) {
+    opt = opt || {};
+    opt.method = opt.method.toUpperCase() || 'POST';
+    //  opt.method = (opt.method==null?"GET":opt.method.toUpperCase());
+    opt.url = opt.url || '';
+    opt.async = opt.async || true;
+    opt.data = opt.data || null;
+    opt.success = opt.success || function() {};
+    var xmlHttp = null;
+    if (XMLHttpRequest) {
+        xmlHttp = new XMLHttpRequest();
+    } else {
+        xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+    }
+    var params = [];
+    for (var key in opt.data) {
+        params.push(key + '=' + opt.data[key]);
+    }
+    var postData = params.join('&');
+    // post
+    if (opt.method.toUpperCase() === 'POST') {
+        xmlHttp.open(opt.method, opt.url, opt.async);
+        xmlHttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=utf-8');
+        xmlHttp.send(postData);
+        // get
+    } else if (opt.method.toUpperCase() === 'GET') {
+        xmlHttp.open(opt.method, opt.url + '?' + postData, opt.async);
+        xmlHttp.send(null);
+    }
+    // loading
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            opt.success(xmlHttp.responseText);
+        }
+    };
+}
+
+// 使用
+ajax({
+    method: 'POST',
+    url: 'test.txt',
+    data: {},
+    success: function(response) {
+        console.log(response);
+    }
+});
